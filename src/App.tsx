ed57b3917b3c8da303656d9622cca133e5eed4bb
Paulo Bordignon <p.h_@outlook.com>
2022-03-04 23:50:31 -0300

    feat(app): testing get route of mock api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-function App() {
+import api from './services/api';
+import { useState, useCallback, useEffect } from 'react';
 
-  return (
-    <div className="App">
-      <h1>create project</h1>
-    </div>
-  )
+type taskItem = {
+  description: string;
+  id: number;
 }
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect (() => {
+    api.get('/tasks').then(response => {
+      setTasks(response?.data)
+    }).catch(err => {
+      console.log('err')
+    })
+ }, [])
+
+const renderItems = useCallback(() =>
+  tasks.map((item: taskItem) =>(
+    <h1> {item.description} </h1>
+  )
+), [tasks]);
+
+return (
+  <div className="App">
+    <h1>testando api</h1>
+    {renderItems()}
+  </div>
+)}
 
 export default App
