bb9797694e752fc9ee2be9ac074ec7f8d1552ebf
Paulo Bordignon <p.h_@outlook.com>
2022-03-05 12:26:40 -0300

    chore(all): added eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import api from './services/api';
 import { useState, useCallback, useEffect } from 'react';
+
+import api from './services/api';
 
 type taskItem = {
   description: string;
   id: number;
-}
+};
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  useEffect (() => {
-    api.get('/tasks').then(response => {
-      setTasks(response?.data)
-    }).catch(err => {
-      console.log('err')
-    })
- }, [])
+  useEffect(() => {
+    api
+      .get('/tasks')
+      .then(response => {
+        setTasks(response?.data);
+      })
+      .catch(err => {
+        console.log('err');
+      });
+  }, []);
 
-const renderItems = useCallback(() =>
-  tasks.map((item: taskItem) =>(
-    <h1> {item.description} </h1>
-  )
-), [tasks]);
+  const renderItems = useCallback(
+    () => tasks.map((item: taskItem) => <h1> {item.description} </h1>),
+    [tasks]
+  );
 
-return (
-  <div className="App">
-    <h1>testando api</h1>
-    {renderItems()}
-  </div>
-)}
+  return (
+    <div className="App">
+      <h1>testando api</h1>
+      {renderItems()}
+    </div>
+  );
+}
 
-export default App
+export default App;
